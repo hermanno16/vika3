211c07d693aa6d1604bd029a5631f749219c6363
SigurdurRWB <sig.ragnar@gmail.com>
2016-12-12 17:29:29 +0000

    addWindow.ui
    Beinagrind fyrir addWindow.ui. Tenging ekki komin á.

---
+++ addwindow.cpp
@@ -0,0 +1,15 @@
+#include "addwindow.h"
+#include "ui_addwindow.h"
+#include <string>
+#include <QString>
+
+using namespace std;
+
+addwindow::addwindow()
+{
+
+}
+void addwindow::addscientist()
+{
+
+}
---
+++ addwindow.h
@@ -0,0 +1,16 @@
+#ifndef ADDWINDOW_H
+#define ADDWINDOW_H
+#include "scientist.h"
+#include "service.h"
+
+#include <QWidget>
+using namespace std;
+
+class addwindow
+{
+public:
+    addwindow();
+    void addscientist();
+};
+
+#endif // ADDWINDOW_H
