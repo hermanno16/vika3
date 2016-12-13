a1a9f79dbb7aad6532a07b356e850fc9318c0c8e
bergthora15 <bergthora15@ru.is>
2016-12-13 21:55:33 +0000

    add photo scientist - reynir

--- addscientistdialog.cpp
+++ addscientistdialog.cpp
@@ -12,3 +12,26 @@ AddScientistDialog::~AddScientistDialog()
 {
     delete ui;
 }
+
+void AddScientistDialog::on_add_Photo_Button_clicked()
+{
+    string filePath = QFileDialog::getOpenFileName(
+                    this,
+                    "Search for images",
+                    "",
+                    "Image files (*.png *.jpg)"
+                    ).toStdString();
+        if (filePath.length())
+        {
+            //user selected some file
+            QPixmap pixmap(QString::fromStdString(filePath));
+
+            ui->photo_scientist_frame->setPixmap(pixmap);
+            ui->photo_scientist_frame->setScaledContents(true);
+
+        }
+        else
+        {
+            //user did not select a file
+        }
+}
--- addscientistdialog.h
+++ addscientistdialog.h
@@ -1,6 +1,12 @@
 #ifndef ADDSCIENTISTDIALOG_H
 #define ADDSCIENTISTDIALOG_H
 #include "service.h"
+#include <QDialog>
+#include <string>
+#include <QString>
+#include <QPixmap>
+#include <QFileDialog>
+#include <QFrame>
 
 #include <QDialog>
 
@@ -15,6 +21,9 @@ class AddScientistDialog : public QDialog
 public:
     explicit AddScientistDialog(QWidget *parent = 0);
     ~AddScientistDialog();
+
+private slots:
+    void on_add_Photo_Button_clicked();
 
 private:
     Ui::AddScientistDialog *ui;
